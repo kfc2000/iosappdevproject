eb6c1f1832a1ea2d7c86a494ae2bc916d88270f7
Chow Kim Foong <kimfoongchow@gmail.com>
2020-10-14 09:53:18 +0800

    First commit

---
+++ iosappdev/iosappdev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosappdev
+//
+//  Created by Specialist Diploma on 14/10/20.
+//  Copyright © 2020 NYP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
